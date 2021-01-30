d3a388859dc9b16ea5cbb88edc58b8723a852365
KaisKermani <kaiskermani@gmail.com>
2021-01-30 23:04:46 +0100

    Page Projet squelette

---
+++ client/src/app/pages/projet-display/projet-display.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import {BASE_API, PROJET} from "../../globals/vars";
+import {CrudService} from '../../services/crud.service';
+
+@Component({
+  selector: 'app-projet-display',
+  templateUrl: './projet-display.component.html',
+  styleUrls: ['./projet-display.component.css']
+})
+export class ProjetDisplayComponent implements OnInit {
+
+  id:number;
+  projet:any;
+
+  constructor(
+    private crudService: CrudService,
+    private route: ActivatedRoute
+  ) {}
+
+  ngOnInit(): void {
+    this.id = this.route.snapshot.params.id;
+    this.projet = {};
+    this.getProjet();
+  }
+
+  getProjet() {
+    this.crudService.getAll(BASE_API + PROJET + '/' + this.id).subscribe(
+      // @ts-ignore
+      (data: any[]) => {
+        this.projet = data;
+        console.log(this.projet);
+      }, (error) => {
+        console.log(error);
+      }
+    );
+  }
+
+}
